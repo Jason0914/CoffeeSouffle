b4978008c467002b739c7267aa747ac1cc79ae46
Jason0914 <a22586203a@gmail.com>
2024-12-17 15:03:44 +0800

    run this

---
+++ run.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Starting room backend server
+java -jar C:\Users\User\Desktop\CoffeeSouffle\Coffeesouffle\target\Coffeesouffle-0.0.1-SNAPSHOT.war
+
+
+echo All jars exec successfully
+pause
