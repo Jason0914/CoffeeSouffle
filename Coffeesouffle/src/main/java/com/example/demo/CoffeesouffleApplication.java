c65f583f79e65081b4bf9b7701142decd5b8d4f8
Jason0914 <a22586203a@gmail.com>
2024-12-20 00:37:34 +0800

    run

--- Coffeesouffle/src/main/java/com/example/demo/CoffeesouffleApplication.java
+++ Coffeesouffle/src/main/java/com/example/demo/CoffeesouffleApplication.java
@@ -1,10 +1,14 @@
 package com.example.demo;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 
 @SpringBootApplication
 @ServletComponentScan
@@ -17,5 +21,15 @@ public class CoffeesouffleApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CoffeesouffleApplication.class, args);
+    }
+    
+    @Bean
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+            .url(System.getenv("MYSQL_URL"))
+            .username(System.getenv("MYSQLUSER"))
+            .password(System.getenv("MYSQLPASSWORD"))
+            .driverClassName("com.mysql.cj.jdbc.Driver")
+            .build();
     }
 }
